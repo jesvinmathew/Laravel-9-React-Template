73f09a9ca329e9a05568e19b3e37715492a3cdd6
jesvinmathew <jesvinmathew@gmail.com>
2022-10-15 17:09:10 +0530

    laravel and react set up completed

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './components/App'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <App />
+)
